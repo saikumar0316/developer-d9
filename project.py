cf0761c08d0753ee8b3f418885b291ccc2ee2753
saikumar0316 <bolinenisaikumar1234@gmail.com>
2026-02-27 14:57:29 +0530

    sweety

--- project.py
+++ project.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 print("ela vunnavu sweety")
 print("friend rice techa ra sweety")
 print("baunava ")
-=======
-
 print("friend rice techa ra sweety")
 print("claps endhuku sweety")
->>>>>>> 88ea751a2840cce5c0cf39419f766198d48b5f3e
